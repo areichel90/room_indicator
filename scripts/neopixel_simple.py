f8426d53239fdb7133950ca8e4332049320a1129
areichel90 <andrew.reichel@gmail.com>
2021-01-02 04:42:01 +0000

    trying to get test to work on pixel

--- scripts/neopixel_simple.py
+++ scripts/neopixel_simple.py
@@ -1,6 +1,9 @@
-import board, neopixel
+import board, neopixel, time
 
-pixels = neopixel.NeoPixel(board.D18, 8)
+pixels = neopixel.NeoPixel(board.D18, 8, brightness=0.2, auto_write=True, pixel_order=neopixel.RGBW)
 
 if __name__ == '__main__':
-    pixels[0] = (255, 0, 0)
+    pixels[0] = (255, 0,0,0)
+
+    time.sleep(30)
+
