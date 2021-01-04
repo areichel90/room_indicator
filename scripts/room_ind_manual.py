ad8041d51908f13d26b5480accc7c48c11e3d327
Andrew Reichel <andrew.reichel@gmail.com>
2021-01-03 21:25:21 -0500

    added initial argparser

--- scripts/room_ind_manual.py
+++ scripts/room_ind_manual.py
@@ -1,13 +1,20 @@
-import time, neopixel, board
+import time, neopixel, board, argparse
 import neopixel_tmp as neopixels
 
 if __name__ == '__main__':
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--in_meeting", help='boolean "in meeting" status', action='store_true', optional=True)
+    parser.add_argument("-f", "--free", help='boolean status for "not in meeting"', action='store_true', optional=True)
+    parser.parse_args()
+
     # initialize neopixel setup
     pixels = neopixels.initialize_neopixels()
 
     # generate random color
     color = neopixels.generate_random_color()
-
     # flash all elements
     neopixels.pixel_flash(pixels=pixels, rgbw = color, verbose=True)
+
+
 
