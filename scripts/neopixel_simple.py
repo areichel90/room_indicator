324d7b634cccd8799ef1253083b00559e4db2e00
Andrew Reichel <andrew.reichel@gmail.com>
2021-01-21 21:05:16 -0500

    slice test_0

--- scripts/neopixel_simple.py
+++ scripts/neopixel_simple.py
@@ -1,9 +1,15 @@
 import board, neopixel, time
 
+# pixel assignment
+leftPanel_elements = ((0,7), (24,31), (32,39), (56,63))
+#left_panel = neopixels.create_panel_list(leftPanel_elements)
+rightPanel_elements = ((8,15), (16,23), (40,47), (48,55))
+#right_panel = neopixels.create_panel_list(rightPanel_elements)
+
 pixels = neopixel.NeoPixel(board.D18, 8, brightness=0.2, auto_write=True, pixel_order=neopixel.RGBW)
 
 if __name__ == '__main__':
-    pixels[0] = (255, 0,0,0)
+    pixels[leftPanel_elements] = (255, 0,0,0)
 
     time.sleep(30)
 
